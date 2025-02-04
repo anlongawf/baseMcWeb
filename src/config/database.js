2340fb17924fe550517bc3d7a156d2f3a258e8b3
anlongawf <88261950+anlongawf@users.noreply.github.com>
2025-02-04 07:45:34 +0700

    update db

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 import 'dotenv/config';
 import mariadb from 'mariadb';
 
-// const pool = mariadb.createPool({
-//     host: process.env.DATABASE_HOST,
-//     user: process.env.DATABASE_USER,
-//     password: process.env.DATABASE_PASSWORD,
-//     database: process.env.DATABASE_NAME,
-//     port: process.env.DATABASE_PORT,
-// });
+const pool = mariadb.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT,
+});
 
 export default pool;
